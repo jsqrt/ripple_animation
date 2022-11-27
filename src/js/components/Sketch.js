6ef24c783e67fc3e992f519ddde121ac00f2abe9
geoshavel <geoshavel@gmail.com>
2022-11-27 16:52:48 +0200

    feat(basic ripple anim)

--- src/js/components/Sketch.js
+++ src/js/components/Sketch.js
@@ -5,9 +5,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import fragment from './shader/fragment.glsl';
 // import vertex from '../../shader/vertex.glsl';
 
+import brush from '../../images/burash01.png';
+import image from '../../images/image.jpeg';
+
 export default class Sketch {
 	constructor(options) {
 		this.scene = new T.Scene();
+		this.sceneSecond = new T.Scene();
 
 		this.container = options.dom;
 		this.width = this.container.offsetWidth;
@@ -15,7 +19,7 @@ export default class Sketch {
 		this.renderer = new T.WebGLRenderer();
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(this.width, this.height);
-		this.renderer.setClearColor(0xeeeeee, 1);
+		this.renderer.setClearColor(0x000000, 1);
 		this.renderer.outputEncoding = T.sRGBEncoding;
 
 		this.container.appendChild(this.renderer.domElement);
@@ -27,16 +31,26 @@ export default class Sketch {
 			1000,
 		);
 
-		// var frustumSize = 10;
-		// var aspect = window.innerWidth / window.innerHeight;
-		// this.camera = new T.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
+		this.baseTexture = new T.WebGLRenderTarget(this.width, this.height, {
+			minFilter: T.LinearFilter,
+			magFilter: T.LinearFilter,
+			format: T.RGBAFormat,
+		});
+
+		const frustumSize = this.height;
+		const aspect = this.width / this.height;
+		this.camera = new T.OrthographicCamera((frustumSize * aspect) / -2, (frustumSize * aspect) / 2, frustumSize / 2, frustumSize / -2, -1000, 1000);
 		this.camera.position.set(0, 0, 2);
 		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 		this.time = 0;
+		this.mouse = new T.Vector2(0, 0);
+		this.prevMouse = new T.Vector2(0, 0);
+		this.currentWave = 0;
 
 		this.isPlaying = true;
 
 		this.loadObjects().then(() => {
+			this.mouseEvents();
 			this.addObjects();
 			this.resize();
 			this.render();
@@ -112,9 +126,11 @@ export default class Sketch {
 			uniforms: {
 				time: { type: 'f', value: 0 },
 				resolution: { type: 'v4', value: new T.Vector4() },
-				uvRate1: {
-					value: new T.Vector2(1, 1),
-				},
+				uDisplacement: { value: null },
+				uTexture: { value: new T.TextureLoader().load(image) },
+				// uvRate1: {
+				// 	value: new T.Vector2(1, 1),
+				// },
 			},
 			// wireframe: true,
 			// transparent: true,
@@ -122,10 +138,31 @@ export default class Sketch {
 			fragmentShader: this.fragment,
 		});
 
-		this.geometry = new T.PlaneGeometry(1, 1, 1, 1);
+		this.maxWaves = 150;
+		this.geometry = new T.PlaneGeometry(86, 86, 1, 1);
+		this.geometryFullscreen = new T.PlaneGeometry(this.width, this.height, 1, 1);
 
-		this.plane = new T.Mesh(this.geometry, this.material);
-		this.scene.add(this.plane);
+		this.meshes = [];
+
+		for (let i = 0; i < this.maxWaves; i += 1) {
+			const material = new T.MeshBasicMaterial({
+				map: new T.TextureLoader().load(brush),
+				transparent: true,
+				blending: T.AdditiveBlending,
+				depthTest: false,
+				depthWrite: false,
+			});
+
+			const mesh = new T.Mesh(this.geometry, material);
+			// mesh.visible = false;
+			mesh.rotation.z = 2 * Math.PI * Math.random();
+
+			this.scene.add(mesh);
+			this.meshes.push(mesh);
+		}
+
+		this.quad = new T.Mesh(this.geometryFullscreen, this.material);
+		this.sceneSecond.add(this.quad);
 	}
 
 	stop() {
@@ -139,11 +176,56 @@ export default class Sketch {
 		}
 	}
 
+	setNewWave(x, y, index) {
+		this.meshes[index].visible = true;
+		this.meshes[index].position.x = x;
+		this.meshes[index].position.y = y;
+		this.meshes[index].material.opacity = 0.5;
+		this.meshes[index].scale.x = 0.15;
+		this.meshes[index].scale.y = 0.15;
+	}
+
+	mouseEvents() {
+		window.addEventListener('mousemove', (e) => {
+			this.mouse.x = e.clientX - this.width / 2;
+			this.mouse.y = this.height / 2 - e.clientY;
+		});
+	}
+
+	trackMousePos() {
+		if (!(Math.abs(this.mouse.x - this.prevMouse.x) < 4 && Math.abs(this.mouse.y - this.prevMouse.y) < 4)) {
+			this.setNewWave(this.mouse.x, this.mouse.y, this.currentWave);
+			this.currentWave = (this.currentWave + 1) % this.maxWaves;
+		}
+		this.prevMouse.x = this.mouse.x;
+		this.prevMouse.y = this.mouse.y;
+	}
+
 	render() {
+		this.trackMousePos();
 		if (!this.isPlaying) return;
 		this.time += 0.05;
-		this.material.uniforms.time.value = this.time;
+		// this.material.uniforms.time.value = this.time;
 		window.requestAnimationFrame(this.render.bind(this));
+
+		this.renderer.setRenderTarget(this.baseTexture);
 		this.renderer.render(this.scene, this.camera);
+		this.material.uniforms.uDisplacement.value = this.baseTexture.texture;
+		this.renderer.setRenderTarget(null);
+		this.renderer.clear();
+		this.renderer.render(this.sceneSecond, this.camera);
+
+		this.meshes.forEach((mesh) => {
+			if (!mesh.visible) return;
+			const m = mesh;
+			// m.position.x = this.mouse.x;
+			// m.position.y = this.mouse.y;
+			m.rotation.z += 0.02;
+			m.material.opacity *= 0.96;
+			m.scale.x = m.scale.x * 0.982 + 0.108;
+			m.scale.y = m.scale.x;
+
+			if (m.material.opacity < 0.002) m.visible = false;
+		});
 	}
 }
